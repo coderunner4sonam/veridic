6a99817a773302a81999c3aa89107d1d244aef75
Sonam <sonammurarkar41@gmail.com>
2023-03-10 08:51:42 +0530

    loader part complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Loading from './Loading';
 
 function App() {
   return (
     <div className="App">
-     
+     <Loading/>
     </div>
   );
 }
---
+++ src/Loading.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+function Loading() {
+    let [loader,setLoader]=useState(true)
+    let [count,setCount]=useState(0)
+
+    let k=null;
+    useEffect(()=>{
+        if(loader===true){
+
+            k= setInterval(()=>{
+                
+                setCount((pre)=>pre=pre+1)
+                
+            },46)
+        }
+
+        return ()=>clearInterval(k)
+    },[count,loader])
+
+  return (
+    <div style={{ height:"10px",
+    backgroundColor:"yellow",
+    width:"100%",
+    display:"flex",
+    borderRadius:"10px"}}>
+      {
+        [...Array(80)].map((element,index)=>(
+            <div style={{backgroundColor:count>index?"green":"white",width:"200px",height:"10px"}} key={index+index}></div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Loading
