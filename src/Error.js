80d283dd25390efbd515a4e9582adaf8e255c234
Sonam <sonammurarkar41@gmail.com>
2023-03-10 09:33:32 +0530

    error part complete

---
+++ src/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center",width:"100vw",height:"100vh",backgroundColor:"#f5f5DC",color:"black",fontSize:"100px"}}>
+      404 Error
+    </div>
+  )
+}
+
+export default Error
